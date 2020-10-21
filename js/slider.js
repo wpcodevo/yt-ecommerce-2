40d302e82bd92de23a41b9cdbe6a1c236d63f1dd
Ziddah Edem <ziddah.edem@gmail.com>
2020-10-21 17:27:49 +0000

    :tada: initial commit

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,6 @@
 const glide1 = document.getElementById("glide1");
+const glide2 = document.getElementById("glide2");
+console.log(glide2);
 
 if (glide1)
   new Glide(glide1, {
@@ -9,4 +11,23 @@ if (glide1)
     perView: 1,
     animationDuration: 800,
     animationTimingFunc: "linear",
+  }).mount();
+
+if (glide2)
+  new Glide(glide2, {
+    type: "carousel",
+    startAt: 0,
+    perView: 5,
+    rewin: false,
+    autoplay: 2000,
+    animationDuration: 800,
+    animationTimingFunc: "cubic-bezier(0.165, 0.840, 0.440, 1.000)",
+    breakpoints: {
+      1200: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+    },
   }).mount();
